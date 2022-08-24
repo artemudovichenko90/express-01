f9f25e720bcc2a80ddd87e97078990e5299a4175
Artem Udovichenko <artemudovichenko90@gmail.com>
2022-08-24 21:14:21 +0300

    Create server

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(123);
+const express = require("express");
+const validate = require("./middleware/validate.mw")
+const UserController = require("./controllers/user.controller")
+
+const app = express();
+const PORT = 3000;
+
+const parse = express.json();
+
+app.get('/users', UserController.showUsers);
+
+app.get('/user/:id', UserController.showUser);
+
+app.post('/user', parse, validate, UserController.createUser);
+
+//app.put('/user/:id', parse, validate, updateUser);
+
+// app.delete('/user/:id', User.Controller.deleteUser);
+
+
+app.listen(PORT, () => {
+  console.log('server started at port ' + PORT);
+});
